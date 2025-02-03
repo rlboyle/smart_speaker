c61415a6c438f560ccb3f05a8a212d9ea7dbc7e9
rlboyle <rlboyle77@gmail.com>
2025-02-02 18:59:47 -0600

    try except

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ import sys
 def text_to_speech(text):
     os.system(f"espeak \"{text}\"")
 
-load_dotenv()
-
-client = Anthropic()
 # message = client.messages.create(
 #     model="claude-3-5-sonnet-20241022",
 #     max_tokens=1024,
@@ -21,18 +18,23 @@ client = Anthropic()
 # print(message.content[0].text)
 
 if __name__ == "__main__":
-    while True:
-        prompt = input("Say something: ")
-        message = client.messages.create(
-            model="claude-3-5-sonnet-20241022",
-            max_tokens=1024,
-            system="You are a helpful smart speaker assistant named Garth. You can assist with my questions and are always positive",
-            messages=[
-                {"role": "user", "content": prompt}
-            ]
-        )
-        text_to_speech(message.content[0].text)
-        # print(message.content[0].text)
+    try:
+        load_dotenv()
+        client = Anthropic()
+        while True:
+            prompt = input("Say something: ")
+            message = client.messages.create(
+                model="claude-3-5-sonnet-20241022",
+                max_tokens=1024,
+                system="You are a helpful smart speaker assistant named Garth. You can assist with my questions and are always positive",
+                messages=[
+                    {"role": "user", "content": prompt}
+                ]
+            )
+            text_to_speech(message.content[0].text)
+            # print(message.content[0].text)
+    except KeyboardInterrupt:
+        print(f"\nExiting...")
     
 
 
