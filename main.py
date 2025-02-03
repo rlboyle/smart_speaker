95aa30852d7bdc5a1618eeaea7b99fb4bb8181bb
rlboyle <rlboyle77@gmail.com>
2025-02-02 19:04:19 -0600

    some more stuff

--- main.py
+++ main.py
@@ -7,16 +7,6 @@ import sys
 def text_to_speech(text):
     os.system(f"espeak \"{text}\"")
 
-# message = client.messages.create(
-#     model="claude-3-5-sonnet-20241022",
-#     max_tokens=1024,
-#     system="You are a helpful smart speaker assistant named Garth. You can assist with my questions and are always positive",
-#     messages=[
-#         {"role": "user", "content": "Who are you?"}
-#     ]
-# )
-# print(message.content[0].text)
-
 if __name__ == "__main__":
     try:
         load_dotenv()
