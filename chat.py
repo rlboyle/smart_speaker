e84b83405444663f6b278c6a4eaa781003a0a7a7
rlboyle <rlboyle77@gmail.com>
2025-02-17 19:25:46 +0000

    anthropic on pi

--- chat.py
+++ chat.py
@@ -2,8 +2,8 @@ from dotenv import load_dotenv
 from anthropic import Anthropic
 import whisper
 # from gtts import gTTS
-import pyttsx3
-from pynput import keyboard
+# import pyttsx3
+# from pynput import keyboard
 import pyaudio
 import wave
 
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     load_dotenv()
     client = Anthropic()
     model = whisper.load_model("tiny")
-    user = input("Talk to Garth")
+    prompt = input("Talk to Garth: ")
     message = client.messages.create(
                     model="claude-3-5-sonnet-latest",
                     max_tokens=256,
@@ -20,6 +20,8 @@ if __name__ == "__main__":
                         you are connected to a speaker and a microphone. You are connected to the internet.
                         You were created for a senior design capstone project by Ryan Boyle, Anna Murray, and Victoria Brown at Northwestern University.
                         You assist me by answering my questions and are always positive. Be concise and accurate with you answers.""",
-                    messages=None
+                        messages=[
+                            {"role": "user", "content": prompt}
+                        ]
                 )
     print(message.content[0].text)
